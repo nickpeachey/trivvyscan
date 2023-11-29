d1212f2d633dd176c120acf02ff145c057ff2e62
nickpeachey <nick.peachey@gmail.com>
2023-11-29 15:38:46 +0000

    now populating jira

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ trivy image -f json -o trivy_results.json $@
 
 #!/bin/bash
 # Pass the results to the Python script
+#pip3 install requests --root-user-action=ignore
+pip install requests --root-user-action=ignore
 python reporter.py trivy_results.json
--- reporter.py
+++ reporter.py
@@ -1,21 +1,59 @@
 # process_results.py
 
-import sys
 import json
+import requests
 
-def process_results(json_file):
-    # Load the JSON results
-    with open(json_file, 'r') as file:
-        results = json.load(file)
+def parse():
+    try:
+        with open('trivy_results.json', 'r') as file:
+            data = json.load(file)
+            results = data['Results']
+            vuls = results[0]['Vulnerabilities']
+            for item in vuls:
+                create_jira_issue(item['Title'], item['Description'])
+                # print(json.dumps(item['Description'], indent=2))
+                # print(json.dumps(item['Title'], indent=2))
+                print('------------')
+    except FileNotFoundError:
+        print(f"Error: File not found")
+    except json.JSONDecodeError:
+        print(f"Error: Unable to parse JSON in file ")
+    except Exception as e:
+        print(f"Error: An unexpected error occurred - {str(e)}")
 
-    # Process the results as needed
-    print("Trivy scan results:")
-    print(json.dumps(results, indent=2))
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 process_results.py <trivy_results.json>")
-        sys.exit(1)
+JIRA_SERVER = "https://npeachey.atlassian.net"
+JIRA_USERNAME = "nick.peachey@googlemail.com"
+JIRA_PASSWORD = "ATATT3xFfGF0W_-zL1fi24SmtsofpnGUWUxMLe9gf-mPhPJkmfcKkwnXoztRmciKW9L3ykhWdfAG7OM0S3w9gI8-8S2hvQ5Vm0IuTYvbmeVF_QCMysqyFNYU4ulojHzgSBGzH53MkEmWSQj6c_sPU2idiZiOzPhptKA9H4AHa3IUq96ZxbP4wx8=985C963A"
+JIRA_PROJECT_KEY = "BET"
+JIRA_ISSUE_TYPE = "Bug"
 
-    json_file = sys.argv[1]
-    process_results(json_file)
+
+def create_jira_issue(summary, description):
+    url = f"{JIRA_SERVER}/rest/api/2/issue/"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    auth = (JIRA_USERNAME, JIRA_PASSWORD)
+    data = {
+        "fields": {
+            "project": {
+                "key": JIRA_PROJECT_KEY
+            },
+            "summary": summary,
+            "description": description,
+            "issuetype": {
+                "name": JIRA_ISSUE_TYPE
+            }
+        }
+    }
+    response = requests.post(url, headers=headers, auth=auth, json=data)
+    if response.status_code == 201:
+        print(f"Jira issue created successfully: {response.json()['key']}")
+    else:
+        print(f"Failed to create Jira issue. Status code: {response.status_code}, Response: {response.text}")
+
+
+
+
+parse()
